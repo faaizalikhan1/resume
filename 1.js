061507e6ee3d40bee00afee55d0910d756c99695
goprime10 <iamfaaizali@gmail.com>
2018-05-23 15:36:17 +0530

    Resume webpage's code

---
+++ 1.js
@@ -0,0 +1,10 @@
+function openNav_Resume() {
+  document.getElementById("ResumeNav").style.width = "35vw";
+  document.getElementById("main").style.marginLeft = "35vw";
+}
+
+function closeNav_Resume() {
+  document.getElementById("ResumeNav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
